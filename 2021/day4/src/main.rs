12976dd7b893bb2856bb5a6d8e715b48131667ac
Kento Okamoto <kentokamoto@protonmail.com>
2022-05-22 19:27:56 -0400

    Proper implementation of clone, adding mark_board

--- 2021/day4/src/main.rs
+++ 2021/day4/src/main.rs
@@ -3,10 +3,46 @@ use std::io::stdin;
 use std::io::{BufRead, BufReader};
 use std::fmt;
 
-fn part1(lines: &Vec<i64>) {}
+fn part1(guesses: &mut Vec<String>, bingo_boards: &Vec<BingoBoard>) {
+    let mut board_copy = bingo_boards.clone();
+    for guess in guesses{
+        println!("Current Guess: {}", guess);
+        for b in &mut board_copy{
+            let mut row = 0;
+            let mut col = 0;
+            if b.has_value(guess, &mut row, &mut col) {
+                println!("Found at {}, {} on board: \n{}", row, col, b);
+                b.mark_board(&row, &col);
+            }
+        }
+    }
+
+}
 fn part2(lines: &Vec<i64>) {}
+
+#[derive(Clone)]
 struct BingoBoard {
-    board: Vec<Vec<i32>>,
+    board: Vec<Vec<String>>,
+}
+impl BingoBoard {
+    fn has_value(&self, val: &String, r: &mut usize, c: &mut usize) -> bool{
+        for row in 0..self.board.len(){
+            for col in 0..self.board[row].len(){
+                if self.board[row][col].eq(val){
+                    *r = row;
+                    *c = col;
+                    return true;
+                }
+            }
+        }
+        false
+    }
+    fn mark_board(&mut self, row: &usize, col: &usize){
+        if self.board[*row][*col].starts_with('x'){
+            return
+        }
+        self.board[*row][*col] = String::from("x") + &self.board[*row][*col];
+    }
 }
 impl fmt::Display for BingoBoard{
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
@@ -35,15 +71,15 @@ fn main() -> std::io::Result<()> {
     buf_reader.read_line(&mut contents)?;
 
     // The first line of the file is for guesses
-    let mut guesses: Vec<i32> = Vec::new();
+    let mut guesses: Vec<String> = Vec::new();
     for i in contents.trim().split(",") {
-        guesses.push(i.parse::<i32>().unwrap());
+        guesses.push(i.to_string());
     }
     contents.clear();
 
     // Create the bingo boards
     let mut bingo_boards: Vec<BingoBoard> = Vec::new();
-    let mut temp_board: Vec<Vec<i32>> = Vec::new();
+    let mut temp_board: Vec<Vec<String>> = Vec::new();
     for line in buf_reader.lines() {
         contents = line.unwrap();
         if contents.trim().len() == 0 {
@@ -51,18 +87,20 @@ fn main() -> std::io::Result<()> {
             bingo_boards.push(temp);
             temp_board = Vec::new();
         } else {
-            let mut row: Vec<i32> = Vec::new();
+            let mut row: Vec<String> = Vec::new();
             for val in contents.split_whitespace() {
-                row.push(val.parse::<i32>().unwrap());
+                row.push(val.to_string());
             }
             temp_board.push(row);
         }
     }
     let temp = BingoBoard { board: temp_board };
     bingo_boards.push(temp);
-    for board in bingo_boards {
-        println!("{:#}", board);
-    }
+
+    part1(&mut guesses, &mut bingo_boards);
+//    for board in bingo_boards {
+//        println!("{:#}", board);
+//    }
     Ok(())
 }
 // --------------------------------------------------------------------------
