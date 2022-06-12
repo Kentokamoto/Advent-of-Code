1610e3901e075aa20a558b16fe0de1548ec48b92
Kento Okamoto <kentokamoto@protonmail.com>
2022-06-11 21:02:42 -0400

    Part 1 and Part 2

--- 2021/day5/src/lib.rs
+++ 2021/day5/src/lib.rs
@@ -1,109 +1,120 @@
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
-#[derive(PartialEq)]
-#[derive(Debug)]
-pub struct Point{
-    x: i32,
-    y: i32
+#[derive(PartialEq, Debug, Eq, Hash)]
+pub struct Point {
+    pub x: i32,
+    pub y: i32,
 }
 
-#[derive(PartialEq)]
-#[derive(Debug)]
-pub struct LineSegment{
-    start: Point,
+#[derive(PartialEq, Debug)]
+pub struct LineSegment {
+    pub start: Point,
     // Inclusive of End
-    end: Point
+    pub end: Point,
 }
-impl LineSegment{
-    pub fn new(input: &String) -> Result<LineSegment, &str>{
-        if input.is_empty(){
+
+impl LineSegment {
+    pub fn new(input: &String) -> Result<LineSegment, &str> {
+        if input.is_empty() {
             return Err("Input is empty");
         }
-        println!("Input: {}", input);
         let valid_input = input.clone();
-        let points: Vec<&str>= valid_input.split("->").collect();
-        if points.len() != 2{
-            return Err("Input did not in format 'x1,y1 -> x2, y2'")
+        let points: Vec<&str> = valid_input.split("->").collect();
+        if points.len() != 2 {
+            return Err("Input did not in format 'x1,y1 -> x2, y2'");
         }
-        println!("Split: {:?}", points);
-        let point1:Vec<&str> = points[0].trim().split(",").collect();
-        let point2:Vec<&str> = points[1].trim().split(",").collect();
-        let p1 = Point{
+        let point1: Vec<&str> = points[0].trim().split(",").collect();
+        let point2: Vec<&str> = points[1].trim().split(",").collect();
+        let p1 = Point {
             x: point1[0].parse::<i32>().unwrap(),
-            y: point1[1].parse::<i32>().unwrap()
+            y: point1[1].parse::<i32>().unwrap(),
         };
-        let p2 = Point{
+        let p2 = Point {
             x: point2[0].parse::<i32>().unwrap(),
-            y: point2[1].parse::<i32>().unwrap()
+            y: point2[1].parse::<i32>().unwrap(),
         };
-        let output = LineSegment{
-            start: p1,
-            end: p2
-        };
+        let output = LineSegment { start: p1, end: p2 };
         Ok(output)
     }
-    pub fn isDiagonal(self) -> bool {
-        (self.start.x == self.end.x )||  ( self.start.y == self.end.y)
+    pub fn is_diagonal(&self) -> bool {
+        (self.start.x != self.end.x) && (self.start.y != self.end.y)
+    }
+    pub fn is_45_diagonal(&self) -> bool {
+        (self.start.x - self.end.x).abs() == (self.start.y - self.end.y).abs()
+    }
+    pub fn is_vertical(&self) -> bool {
+        (self.start.x == self.end.x) && (self.start.y != self.end.y)
+    }
+    pub fn is_horizontal(&self) -> bool {
+        (self.start.x != self.end.x) && (self.start.y == self.end.y)
     }
 }
-pub fn new(file_name: &String) -> Result<Vec<LineSegment>, &str>{
+pub fn new(file_name: &String) -> Result<Vec<LineSegment>, &str> {
     let file = match File::open(&file_name.trim()) {
         Ok(f) => f,
-        Err(error) => panic!("Problem opening the file: {:?}", error), };
+        Err(error) => panic!("Problem opening the file: {:?}", error),
+    };
     let buf_reader = BufReader::new(file);
 
     let mut output = Vec::new();
-    for line in buf_reader.lines(){
+    for line in buf_reader.lines() {
         output.push(LineSegment::new(&line.unwrap()).unwrap());
     }
     Ok(output)
 }
 
 #[cfg(test)]
-mod tests{
+mod tests {
     use super::*;
 
     #[test]
-    fn create_line_segment(){
+    fn create_line_segment() {
         let input = String::from("3,4 -> 1,4");
         let ls = LineSegment::new(&input).unwrap();
         let answer = LineSegment {
-            start: Point {
-                x: 3,
-                y: 4
-            },
-            end: Point{
-                x: 1,
-                y: 4
-            }
+            start: Point { x: 3, y: 4 },
+            end: Point { x: 1, y: 4 },
         };
         assert_eq!(answer, ls);
     }
     #[test]
-    fn check_diagonals(){
+    fn check_diagonals() {
         let test1 = LineSegment {
-            start: Point {
-                x: 0,
-                y: 4
-            },
-            end: Point{
-                x: 4,
-                y: 0
-            }
+            start: Point { x: 0, y: 4 },
+            end: Point { x: 4, y: 0 },
         };
-        assert_eq!(false, test1.isDiagonal());
+        assert_eq!(true, test1.is_diagonal());
         let test2 = LineSegment {
-            start: Point {
-                x: 3,
-                y: 4
-            },
-            end: Point{
-                x: 1,
-                y: 4
-            }
+            start: Point { x: 3, y: 4 },
+            end: Point { x: 1, y: 4 },
         };
-        assert_eq!(true, test2.isDiagonal());
+        assert_eq!(false, test2.is_diagonal());
+        let test3 = LineSegment {
+            start: Point { x: 0, y: 8 },
+            end: Point { x: 8, y: 0 },
+        };
+        assert_eq!(true, test3.is_45_diagonal());
+        let test4 = LineSegment {
+            start: Point { x: 1, y: 8 },
+            end: Point { x: 8, y: 0 },
+        };
+        assert_eq!(false, test4.is_45_diagonal());
+    }
+    #[test]
+    fn check_vertical() {
+        let test1 = LineSegment {
+            start: Point { x: 0, y: 4 },
+            end: Point { x: 0, y: 9 },
+        };
+        assert_eq!(true, test1.is_vertical());
+    }
+    #[test]
+    fn check_horizontal() {
+        let test1 = LineSegment {
+            start: Point { x: 0, y: 4 },
+            end: Point { x: 9, y: 4 },
+        };
+        assert_eq!(true, test1.is_horizontal());
     }
 }
-
--- 2021/day5/src/main.rs
+++ 2021/day5/src/main.rs
@@ -1,14 +1,108 @@
+use std::cmp::{max, min};
+use std::collections::HashMap;
 use std::io::stdin;
 
-use day5::LineSegment;
+use day5::{LineSegment, Point};
 
-fn main() -> std::io::Result<()>{
+fn part1(segments: &Vec<LineSegment>) -> Result<(), &str> {
+    let part1_segments = segments.clone();
+    let mut vents: HashMap<Point, i32> = HashMap::new();
+    for segment in part1_segments {
+        if segment.is_diagonal() {
+            continue;
+        }
+        if segment.is_vertical() {
+            let max = max(segment.start.y, segment.end.y);
+            let min = min(segment.start.y, segment.end.y);
+            for y in min..max + 1 {
+                let tmp_point = Point {
+                    x: segment.start.x,
+                    y: y,
+                };
+                let count = vents.entry(tmp_point).or_insert(0);
+                *count += 1;
+            }
+        }
+        if segment.is_horizontal() {
+            let max = max(segment.start.x, segment.end.x);
+            let min = min(segment.start.x, segment.end.x);
+            for x in min..max + 1 {
+                let tmp_point = Point {
+                    x: x,
+                    y: segment.start.y,
+                };
+                let count = vents.entry(tmp_point).or_insert(0);
+                *count += 1;
+            }
+        }
+    }
+    let mut counter = 0;
+    for vent in vents {
+        if vent.1 >= 2 {
+            counter += 1;
+        }
+    }
+    println!("Points: {}", counter);
+    Ok(())
+}
+
+fn part2(segments: &Vec<LineSegment>) -> Result<(), &str> {
+    let part2_segments = segments.clone();
+    let mut vents: HashMap<Point, i32> = HashMap::new();
+    for segment in part2_segments {
+        if segment.is_45_diagonal() {
+            let count = (segment.start.x - segment.end.x).abs();
+            let polarity_y = if segment.start.y > segment.end.y {-1} else {1}; 
+            let polarity_x = if segment.start.x > segment.end.x {-1} else {1}; 
+            for i in 0..count+1 {
+                let tmp_point = Point {
+                    x: segment.start.x + (polarity_x* i),
+                    y: segment.start.y + (polarity_y* i),
+                };
+                let count = vents.entry(tmp_point).or_insert(0);
+                *count += 1;
+            } 
+        } else if segment.is_vertical() {
+            let max = max(segment.start.y, segment.end.y);
+            let min = min(segment.start.y, segment.end.y);
+            for y in min..max + 1 {
+                let tmp_point = Point {
+                    x: segment.start.x,
+                    y: y,
+                };
+                let count = vents.entry(tmp_point).or_insert(0);
+                *count += 1;
+            }
+        } else if segment.is_horizontal() {
+            let max = max(segment.start.x, segment.end.x);
+            let min = min(segment.start.x, segment.end.x);
+            for x in min..max + 1 {
+                let tmp_point = Point {
+                    x: x,
+                    y: segment.start.y,
+                };
+                let count = vents.entry(tmp_point).or_insert(0);
+                *count += 1;
+            }
+        }
+    }
+    let mut counter = 0;
+    for vent in vents {
+        if vent.1 >= 2 {
+            counter += 1;
+        }
+    }
+
+    println!("Points: {}", counter);
+    Ok(())
+}
+fn main() -> std::io::Result<()> {
     println!("Enter the name of the file");
     let mut buffer = String::new();
     let stdin = stdin();
-    stdin.read_line(&mut buffer)?;
-    let line_segments = day5::new(&buffer); 
-    println!("Line Segments: {:?}", line_segments);
-
+    stdin.read_line(&mut buffer).unwrap();
+    let line_segments = day5::new(&buffer).unwrap();
+    part1(&line_segments).unwrap();
+    part2(&line_segments).unwrap();
     Ok(())
 }
